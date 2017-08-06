cff046ccd5d289afe7c15dd0f55c114c33368ef6
Shoma Nishitateno <shoma416@gmail.com>
2017-08-06 13:51:36 +0900

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loaders: 'url-loader'
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.css'],
+    modules: [
+      'node_modules'
+    ],
+    alias: {
+      vue: 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"production"'
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true,
+    compress: {
+      warnings: false
+    }
+  }),
+  new webpack.LoaderOptionsPlugin({
+    minimize: true
+  })
+])
+}
