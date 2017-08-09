e28b32158111a0001ad10fcb68245e4ae3ebec0d
Shoma Nishitateno <shoma416@gmail.com>
2017-08-09 18:41:13 +0900

    Updatable post

--- src/components/views/posts/Edit.vue
+++ src/components/views/posts/Edit.vue
@@ -9,14 +9,20 @@
             <div v-if="post">
               <div class="box-body">
 
-              <h4>Title</h4>
-              <div class="input-group">
-                <input class="form-control" type="text" v-model="post.title">
-                <span class="input-group-addon"></span>
-              </div>
-              <br>
+                <h4>Title</h4>
+                <div class="input-group">
+                  <input class="form-control" type="text" v-model="post.title">
+                  <span class="input-group-addon"></span>
+                </div>
+                <br>
 
-
+                <h4>Content</h4>
+                <div class="input-group">
+                  <textarea class="form-control" v-model="post.content"></textarea>
+                  <span class="input-group-addon"></span>
+                </div>
+                <br>
+                <button type="button" class="btn btn-default btn-lg" v-on:click="updatePost()">Update</button>
               </div> <!-- /.box-body -->
             </div>
           </div>
@@ -28,7 +34,7 @@
 
 <script>
     
-</script>export default {
+export default {
   name: 'Settings',
   data () {
     return {
@@ -62,6 +68,21 @@
         this.error = error.response.statusText
       })
     },
+    updatePost() {
+      this.axios.post('/posts', this.post)
+        .then(response => {
+        
+        if (response.status !== 200) {
+            this.error = response.statusText
+            return
+        }
+      })
+      .catch(error => {
+        // Request failed.
+        console.log('error', error.response)
+        this.error = error.response.statusText
+      })
+    }
   },
   mounted () {
     this.getPost()
@@ -70,8 +91,5 @@
 </script>
 
 <style>
-.datetime-picker input {
-  height: 4em !important;
-}
 
 </style>
