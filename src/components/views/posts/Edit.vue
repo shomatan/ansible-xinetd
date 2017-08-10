1e4651b7391abab1e69437fdb7a029260e2adfb8
Shoma Nishitateno <shoma416@gmail.com>
2017-08-10 10:32:00 +0900

    Refactor 2columns

--- src/components/views/posts/Edit.vue
+++ src/components/views/posts/Edit.vue
@@ -3,7 +3,9 @@
     <h1 class="text-center">Edit</h1>
     <section class="content">
       <div class="row">
-        <div class="col-md-12">
+
+        <!-- Edit form -->
+        <div class="col-md-8">
           <div class="box box-info">
 
             <div v-if="post">
@@ -27,13 +29,41 @@
             </div>
           </div>
         </div>
-      </div>
+
+        <!-- Categories -->
+        <div class="col-md-4">
+          <div class="box box-info">
+
+            <div v-if="post">
+              <div class="box-body">
+
+                <h4>Title</h4>
+                <div class="input-group">
+                  <input class="form-control" type="text" v-model="post.title">
+                  <span class="input-group-addon"></span>
+                </div>
+                <br>
+
+                <h4>Content</h4>
+                <div class="input-group">
+                  <textarea class="form-control" v-model="post.content"></textarea>
+                  <span class="input-group-addon"></span>
+                </div>
+                <br>
+                <button type="button" class="btn btn-default btn-lg" v-on:click="updatePost()">Update</button>
+              </div> <!-- /.box-body -->
+            </div>
+          </div>
+        </div>
+
+      </div><!-- /.row -->
+
     </section>
   </div>
 </template>
 
 <script>
-    
+
 export default {
   name: 'Settings',
   data () {
@@ -54,7 +84,7 @@ export default {
     getPost () {
       this.$Progress.start()
       this.axios.get('/posts/' + this.$route.params.id).then( response => {
-        
+
         if (response.status !== 200) {
           this.$Progress.fail()
           this.error = response.statusText
@@ -74,7 +104,7 @@ export default {
     updatePost() {
       this.$Progress.start()
       this.axios.post('/posts', this.post).then(response => {
-        
+
         if (response.status !== 200) {
           this.$Progress.fail()
           this.error = response.statusText
