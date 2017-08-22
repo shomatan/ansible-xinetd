7e9d6b53065fd9b122bf2290d4030c3619628ab0
Shoma Nishitateno <shoma416@gmail.com>
2017-08-22 19:36:20 +0900

    Add sidebar

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,9 +1,32 @@
 <template>
-  <div class="hello">
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <router-view></router-view>
-    </div><!-- /.content-wrapper -->
+  <div class="container">
+    <div class="columns">
+
+      <div class="column is-2 aside is-fullheight is-hidden-mobile">
+        <p class="menu-label">
+          General
+        </p>
+        <ul class="menu-list">
+          <li><router-link to="/">Dashboard</router-link></li>
+        </ul>
+        <p class="menu-label">
+          Posts
+        </p>
+        <ul class="menu-list">
+          <li><router-link to="/posts/create">New Post</router-link></li>
+          <li><router-link to="/posts">List</router-link></li>
+          <li><a>Category</a></li>
+          <li><a>Tag</a></li>
+        </ul>
+      </div>
+
+      <!-- Content Wrapper. Contains page content -->
+      <div class="column is-10">
+        <router-view></router-view>
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
   </div>
 </template>
 
@@ -12,7 +35,17 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
 }
